988ee165816e24fb1c04f05b8faf475812e184f9
Xuyang Hu <thunderlast@gmail.com>
2015-09-07 00:05:03 +0800

    Add center blooming

--- DCPathButton/Classes/DCPathButton.h
+++ DCPathButton/Classes/DCPathButton.h
@@ -24,6 +24,7 @@ typedef NS_ENUM(NSUInteger, kDCPathButtonBloomDirection) {
     kDCPathButtonBloomDirectionBottomRight = 6,
     kDCPathButtonBloomDirectionRight = 7,
     kDCPathButtonBloomDirectionTopRight = 8,
+    kDCPathButtonBloomDirectionCenter = 9,
     
 };
 
