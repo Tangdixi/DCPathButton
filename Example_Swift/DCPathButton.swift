39e1987a9ef4bccab7d63478f60a4ee5a91ecdef
tang dixi <dixi.tang@bindo.com>
2016-11-27 10:21:19 +0800

    Swift Version in progress

---
+++ Example_Swift/DCPathButton.swift
@@ -0,0 +1,18 @@
+//
+//  DCPathButton.swift
+//  Example_Swift
+//
+//  Created by tang dixi on 26/11/2016.
+//  Copyright © 2016 Tangdixi. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+protocol DCPathButton {}
+
+extension DCPathButton where Self: UITabBarController {
+
+	
+	
+}
---
+++ Example_Swift/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Example_Swift
+//
+//  Created by tang dixi on 26/11/2016.
+//  Copyright © 2016 Tangdixi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view, typically from a nib.
+	}
+
+	override func didReceiveMemoryWarning() {
+		super.didReceiveMemoryWarning()
+		// Dispose of any resources that can be recreated.
+	}
+
+
+}
+
